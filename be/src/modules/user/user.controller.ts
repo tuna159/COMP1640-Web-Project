16756c6734b3a2eae5f4aad55fd74f08c21b9b27
Vuong Vu <vuongvu061101@gmail.com>
2023-04-18 22:19:28 +0700

    be fix bug

--- be/src/modules/user/user.controller.ts
+++ be/src/modules/user/user.controller.ts
@@ -1,19 +1,19 @@
 import { UserData } from '@core/decorator/user.decorator';
 import { IUserData } from '@core/interface/default.interface';
-import { UserDetailService } from '@modules/user-detail/user-detail.service';
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { VSignUp } from 'global/dto/createAccount';
 import { VUpdateAccount } from 'global/dto/update-account.dto';
 import { VLogin } from 'global/user/dto/login.dto';
 import { UserService } from './user.service';
+import { Public } from '@core/decorator/public.decorator';
 
 @Controller('user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
-    private readonly userDetailService: UserDetailService,
   ) {}
 
+  @Public()
   @Post('login')
   async login(@Body() body: VLogin) {
     return this.userService.login(body);
